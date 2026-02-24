baf97e527fc94e0e9ce01107db675579781ee277
Marion Calpena <37837069+marioncalpe@users.noreply.github.com>
2026-02-24 11:49:34 +0100

    ajout 'derniere sorti' sur la page home + ajout titre pour les 'sorties du mois' sur la page calendrier

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,19 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import { CommonModule, DecimalPipe } from '@angular/common';
 import { MenuComponent } from '../../shared/components/menu/menu.component';
 import { MotivationBannerComponent } from '../../shared/components/motivation-banner/motivation-banner.component';
+import { Sortie } from '../../core/models/sortie.model';
+import { StorageService } from '../../core/storage/storage.service';
+import { Objectif } from '../../core/models/objectif.model';
 
 @Component({
-    selector: 'app-home',
-    standalone: true,
-    templateUrl: './home.component.html',
-    styleUrls: ['./home.component.scss'],
-    imports: [MenuComponent, MotivationBannerComponent]
+  selector: 'app-home',
+  standalone: true,
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss'],
+  imports: [
+    MenuComponent,
+    MotivationBannerComponent,
+    DecimalPipe,
+    CommonModule,
+  ],
 })
 export class HomeComponent implements OnInit {
+  sorties: Sortie[] = [];
+  recentesSorties: Sortie[] = [];
 
-  constructor() { }
+  constructor(private storageService: StorageService) {}
 
   ngOnInit() {
+    // 5 sorties les plus récentes pour l'historique
+    this.sorties = this.storageService.getSorties();
+    this.recentesSorties = [...this.sorties]
+      .sort((a, b) => new Date(b.start).getTime() - new Date(a.start).getTime())
+      .slice(0, 5);
   }
-
 }
