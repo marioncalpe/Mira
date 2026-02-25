ada346b6a0f6851fa752666e3d725bf215e74e89
Marion Calpena <37837069+marioncalpe@users.noreply.github.com>
2026-02-25 17:22:10 +0100

    affichage dernier badge débloqué page home

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule, DecimalPipe } from '@angular/common';
+
 import { MenuComponent } from '../../shared/components/menu/menu.component';
 import { MotivationBannerComponent } from '../../shared/components/motivation-banner/motivation-banner.component';
 import { Sortie } from '../../core/models/sortie.model';
 import { StorageService } from '../../core/storage/storage.service';
 import { Objectif } from '../../core/models/objectif.model';
+import { Badge } from '../../core/models/badge.model';
 
 @Component({
   selector: 'app-home',
@@ -21,6 +23,7 @@ import { Objectif } from '../../core/models/objectif.model';
 export class HomeComponent implements OnInit {
   sorties: Sortie[] = [];
   recentesSorties: Sortie[] = [];
+  badges: Badge[] = [];
 
   constructor(private storageService: StorageService) {}
 
@@ -30,5 +33,15 @@ export class HomeComponent implements OnInit {
     this.recentesSorties = [...this.sorties]
       .sort((a, b) => new Date(b.start).getTime() - new Date(a.start).getTime())
       .slice(0, 5);
+
+    this.storageService.badges$.subscribe((badges) => {
+      this.badges = badges;
+    });
   }
+  lastBadges(): Badge[] {
+    return [...this.badges]
+      .sort((a, b) => new Date(b.dateUnlocked ?? 0).getTime() - new Date(a.dateUnlocked ?? 0).getTime())
+      .slice(0, 3);
+  }
+  
 }
